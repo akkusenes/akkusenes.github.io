d944ecd2f9aa31b57011f0318ffd3ecd498db8bc
enesvct <116295097+enesvct@users.noreply.github.com>
2023-06-19 02:30:41 +0300

    AddBlog

---
+++ wwwroot/js/form.js
@@ -0,0 +1,53 @@
+﻿var element = document.getElementById('phone');
+var maskOptions = {
+    mask: '+{90} (000) 000 00 00'
+};
+var mask = IMask(element, maskOptions);
+
+var submitButton = $('#submitButton')
+var form = $('#contactForm');
+
+var errorMessage = $('#submitErrorMessage');
+var succesMessage = $('#submitSuccessMessage');
+var tokenField = $('input[name="ReCaptchaToken"]');
+
+
+
+grecaptcha.ready(function () {
+
+    form.on('submit', function (e) {
+        e.preventDefault();
+        if (form.hasClass('loading')) {
+            return;
+        }
+
+        succesMessage.addClass('d-none');
+        errorMessage.addClass('d-none');
+        form.addClass('loading');
+        submitButton.addClass('disable');
+
+
+        grecaptcha.execute('6LeQPIomAAAAAL5oj7w-VT8W6uIHgkavhUNA5bvD', { action: 'submit' }).then(function (token) {
+            tokenField.val(token);
+
+            $.post('/form-gonder',
+                form.serialize(),
+                function (r) {
+                    if (r.success) {
+                        form.removeClass('loading');
+                        submitButton.removeClass('disable');
+                        form[0].reset();
+
+                        errorMessage.removeClass('d-none');
+
+                    } else {
+                        succesMessage.removeClass('d-none');
+                    }
+                }
+            );
+
+        });
+
+
+    });
+});
